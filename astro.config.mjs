88388e8d9391ae4fb8c657e9b09f7ca980d4d7bb
Peter Ehrenberg <pe@dipe.de>
2026-03-02 22:12:40 +0100

    chore: finishing startover with astro...

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://dipe.github.io',
+	integrations: [
+		starlight({
+			title: '[dipe]',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/dipe/dipe.github.io' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
