8dc91afd849949c782c9f9128640abd55a4d7fa2
Peter Ehrenberg <pe@dipe.de>
2026-04-28 23:03:16 +0200

    chore: Cleanup example content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightImageZoom from 'starlight-image-zoom'
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,20 +9,21 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: '[dipe]',
+			plugins: [starlightImageZoom()],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/dipe/dipe.github.io' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+			// sidebar: [
+			// 	{
+			// 		label: 'Guides',
+			// 		items: [
+			// 			// Each item here is one entry in the navigation menu.
+			// 			{ label: 'Example Guide', slug: 'guides/example' },
+			// 		],
+			// 	},
+			// 	{
+			// 		label: 'Reference',
+			// 		autogenerate: { directory: 'reference' },
+			// 	},
+			// ],
 		}),
 	],
 });
